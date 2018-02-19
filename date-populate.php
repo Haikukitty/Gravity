3b1440b11ff6cd9c417fed6f8158a65791bc8541
Haikukitty <Haikukitty@users.noreply.github.com>
2018-02-19 14:40:34 -0500

    Update date-populate.php

--- date-populate.php
+++ date-populate.php
@@ -30,15 +30,16 @@ function ups( $form ) {
         }
  
         // an array of dates as a string (could also be pulled from a DB table)
-$upss = array('2/2/2018','2/9/2018','2/16/2018','2/23/2018','3/2/2018','3/9/2018','3/16/2018','3/23/2018','3/30/2018','4/6/2018','4/13/2018','4/20/2018','4/27/2018','5/4/2018','5/11/2018','5/18/2018','5/25/2018','6/1/2018','6/8/2018','6/15/2018','6/22/2018','6/29/2018','7/6/2018','7/13/2018','7/20/2018','7/27/2018','8/3/2018','8/10/2018','8/17/2018','8/24/2018','8/31/2018','9/7/2018','9/14/2018','9/21/2018','9/28/2018','10/5/2018','10/12/2018','10/19/2018','10/26/2018','11/2/2018','11/9/2018','11/16/2018','11/30/2018','12/7/2018','12/14/2018','12/21/2018','12/28/2018');
+	$upss = array('2/2/2018','2/9/2018','2/16/2018','2/23/2018','3/2/2018','3/9/2018','3/16/2018','3/23/2018','3/30/2018','4/6/2018','4/13/2018','4/20/2018','4/27/2018','5/4/2018','5/11/2018','5/18/2018','5/25/2018','6/1/2018','6/8/2018','6/15/2018','6/22/2018','6/29/2018','7/6/2018','7/13/2018','7/20/2018','7/27/2018','8/3/2018','8/10/2018','8/17/2018','8/24/2018','8/31/2018','9/7/2018','9/14/2018','9/21/2018','9/28/2018','10/5/2018','10/12/2018','10/19/2018','10/26/2018','11/2/2018','11/9/2018','11/16/2018','11/30/2018','12/7/2018','12/14/2018','12/21/2018','12/28/2018');
 		
-		$dtups = new DateTime(); //current Date
+	$dtups = new DateTime(); //current Date
 	$dtups->modify('+7 day'); //If you want to add a buffer to current date (i.e. don't show a date less than 7 days in the future)
 		
 				
-		foreach ($upss as $ups){
+	foreach ($upss as $ups){
 
         $converted_date = DateTime::createFromFormat('n/j/Y', $ups); //convert the string date to DateTime format
+	//you must specify format used in the array - in this case - month/day/year with no leading zeros ('n/j/Y')
 
 			 //compare date in array to current Date
     		if ($converted_date >= $dtups){
@@ -55,21 +56,19 @@ $upss = array('2/2/2018','2/9/2018','2/16/2018','2/23/2018','3/2/2018','3/9/2018
 }
  
         $choices = array();
- //loop through the new/filtered array of dates to dynamically populate the dropdown
+ 	//loop through the new/filtered array of dates to dynamically populate the dropdown
         foreach ( $newupss as $newups ) {
             $choices[] = array( 'text' => $newups, 'value' => $newups );
         }
  
-        // update 'Select Date' to whatever you'd like the instructive option to be
-        $field->placeholder = 'Select a Date';
-        $field->choices = $choices;
+ 	// update 'Select Date' to whatever you'd like the instructive option to be
+ 	$field->placeholder = 'Select a Date';
+ 	$field->choices = $choices;
  
-    }
+   }
  
     return $form;
 }
-
-
 
 
 ?>
